b6ed7122430a2c8c8890a4595e5104a8add746ff
B-Ozturk <302938654@student.rocmondriaan.nl>
2023-01-02 16:25:03 +0100

    Javascript test

--- public/js/coupon.js
+++ public/js/coupon.js
@@ -1,1 +1,10 @@
-console.warn("Javscript werkt op iedere pagina")
+// console.warn("Javscript werkt op iedere pagina");
+
+var now = new Date();
+var delay = 60 * 60 * 1000; // 1 hour in msec
+var start = delay - (now.getMinutes() * 60 + now.getSeconds()) * 1000 + now.getMilliseconds();
+
+setTimeout(function doSomething() {
+    console.warn("uurtje factuurtje")
+    setTimeout(doSomething, delay);
+}, start);
