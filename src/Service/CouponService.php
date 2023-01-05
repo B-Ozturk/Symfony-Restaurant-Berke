bc5a27098f802bb567cf64446497771030bc7cd1
B-Ozturk <302938654@student.rocmondriaan.nl>
2023-01-05 14:10:51 +0100

    service checks for active season

--- src/Service/CouponService.php
+++ src/Service/CouponService.php
@@ -63,8 +63,6 @@ class CouponService
                     $this->entityManagerInterface->remove($coupon);
                     $this->entityManagerInterface->flush();
                 }
-            } else {
-
             }
         }
     }
