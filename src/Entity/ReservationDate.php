588bd4fdf7ecc43cbe425b1aba130d601a766785
B-Ozturk <302938654@student.rocmondriaan.nl>
2023-01-12 14:58:34 +0100

    ReservationDate aangemaakt

---
+++ src/Entity/ReservationDate.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationDateRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReservationDateRepository::class)]
+class ReservationDate
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column]
+    private ?int $spots = null;
+
+    #[ORM\Column]
+    private ?bool $available = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getSpots(): ?int
+    {
+        return $this->spots;
+    }
+
+    public function setSpots(int $spots): self
+    {
+        $this->spots = $spots;
+
+        return $this;
+    }
+
+    public function isAvailable(): ?bool
+    {
+        return $this->available;
+    }
+
+    public function setAvailable(bool $available): self
+    {
+        $this->available = $available;
+
+        return $this;
+    }
+}
